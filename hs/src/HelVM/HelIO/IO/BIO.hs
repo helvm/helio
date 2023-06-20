e7717bbba066c5316ddfc5fabceaa7cc7fd0b824
Kamil Adam <kamil.adam.zabinski@gmail.com>
2023-06-20 20:53:35 +0200

    Fix BIO

--- hs/src/HelVM/HelIO/IO/BIO.hs
+++ hs/src/HelVM/HelIO/IO/BIO.hs
@@ -4,4 +4,4 @@ import           HelVM.HelIO.Control.Business
 import           HelVM.HelIO.IO.Console
 import           HelVM.HelIO.IO.FileReader
 
-type BIO m = (MonadBusiness m , ConsoleIO m , FileReaderIO)
+type BIO m = (MonadBusiness m , ConsoleIO m , FileReaderIO m)
