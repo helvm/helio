3ad445593fd17f016f0c56df9287eff6c25d82eb
Kamil Adam <kamil.adam.zabinski@gmail.com>
2025-08-26 19:59:13 +0200

    Use tasty

--- hs/test/Main.hs
+++ hs/test/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
 import qualified Spec
-import           Test.Hspec      (hspec)
-import           Test.Hspec.Slow
+
+--import           Test.Hspec      (hspec)
+--import           Test.Hspec.Slow
+
+import Test.Tasty
+import Test.Tasty.Hspec
 
 main :: IO ()
-main = (hspec . flip timeThese Spec.spec) =<< configure 1
+main = do
+  hspecTests <- testSpec "Hspec tests" Spec.spec
+  defaultMain (testGroup "All tests" [hspecTests])
+
+--main = (hspec . flip timeThese Spec.spec) =<< configure 1
