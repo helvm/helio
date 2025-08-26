59b9a8cdb3a899ce4d496d087cf3713992c4c1a5
Kamil Adam <kamil.adam.zabinski@gmail.com>
2025-08-26 20:10:31 +0200

    Use hspec-slow

--- hs/test/Main.hs
+++ hs/test/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import           Slow
 import qualified Spec
-import           Test.Hspec (hspec)
+import           Test.Hspec      (hspec)
+import           Test.Hspec.Slow
 
 main :: IO ()
 main = (hspec . flip timeThese Spec.spec) =<< configure 1
