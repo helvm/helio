4939e048223ae0bd2520fb4023851fdc94dc669c
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-08-01 18:27:56 +0200

    Refactor MTIndexSafe

--- hs/src/HelVM/HelIO/Containers/MTIndexSafe.hs
+++ hs/src/HelVM/HelIO/Containers/MTIndexSafe.hs
@@ -11,19 +11,19 @@ import           Prelude                  hiding (break, divMod, drop, fromList,
 
 -- | Index
 naturalIndexSafe :: (MonadSafe m , IndexSafe seq , Num $ Index seq) => seq -> Natural -> m $ Element seq
-naturalIndexSafe l =  indexSafe l <$> fromIntegral
+naturalIndexSafe l = indexSafe l . fromIntegral
 
 -- | Type Class
 class IndexSafe seq where
   findWithDefault :: Element seq -> Index seq -> seq -> Element seq
-  findMaybe  :: Index seq -> seq -> Maybe $ Element seq
-  indexMaybe :: seq -> Index seq -> Maybe $ Element seq
-  findSafe   :: MonadSafe m => Index seq -> seq -> m $ Element seq
-  indexSafe  :: MonadSafe m => seq -> Index seq -> m $ Element seq
+  findMaybe       :: Index seq -> seq -> Maybe $ Element seq
+  indexMaybe      :: seq -> Index seq -> Maybe $ Element seq
+  findSafe        :: MonadSafe m => Index seq -> seq -> m $ Element seq
+  indexSafe       :: MonadSafe m => seq -> Index seq -> m $ Element seq
 
 instance IsSequence seq => IndexSafe seq where
-  findWithDefault e i = fromMaybe e <$> findMaybe i
+  findWithDefault e i = fromMaybe e . findMaybe i
   findMaybe           = flip indexMaybe
-  indexMaybe      l   = rightToMaybe <$> indexSafe l
+  indexMaybe          = index
   findSafe            = flip indexSafe
-  indexSafe       l   = liftMaybeOrError "MTIndexSafe.indexSafe:" <$> index l
+  indexSafe         l = liftMaybeOrError "MTIndexSafe.indexSafe:" . index l
